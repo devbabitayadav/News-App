11c62b11d95854892f208fffd93ed426b7e8d46d
“devbabitayadav” <babitayadav9584@gmail.com>
2023-10-24 17:41:06 +0530

    second commit

--- src/context/news/NewsActions.jsx
+++ src/context/news/NewsActions.jsx
@@ -1,6 +1,5 @@
 export const getNews = async (topic) => {
   const response = await fetch(`https://newsapi.org/v2/everything?q=${topic}&from=2023-10-17&sortBy=publishedAt&apiKey=70d1cfbec9ec452a9585739ab4e8b11e`);
   const data = await response.json();
-  console.log(data);
   return data;
 };
